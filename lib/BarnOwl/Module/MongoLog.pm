cba23dbadee311a184ea2ec4388a296acdc5adbe
Evan Broder <evan@ebroder.net>
2011-04-11 02:10:16 -0700

    More fields cleanup.
    - Remove isprivate and isauto (both duplicate other information)
    - Cast should_wordwrap and private to booleans
    - Lossily cast auth to a bool
    - Clean up uninformative "login" field

--- lib/BarnOwl/Module/MongoLog.pm
+++ lib/BarnOwl/Module/MongoLog.pm
@@ -8,6 +8,7 @@ our $VERSION = 0.1;
 use BarnOwl;
 use BarnOwl::Hooks;
 
+use boolean;
 use DateTime;
 use MongoDB;
 
@@ -33,6 +34,11 @@ sub config {
     $messages = $db->messages;
 }
 
+sub to_boolean {
+    return not(defined $_[0]) ? false :
+        "$_[0]" ? true : false;
+}
+
 sub handle_message {
     my $m = shift;
     if (!$messages) {
@@ -44,9 +50,27 @@ sub handle_message {
     delete $m->{'id'};
     delete $m->{'deleted'};
     delete $m->{'zwriteline'};
+    delete $m->{'isprivate'};
+    delete $m->{'isauto'};
     if (exists($m->{'unix_time'})) {
         $m->{'time'} = DateTime->from_epoch(epoch => $m->{'unix_time'});
         delete $m->{'unix_time'};
+    }
+
+    foreach (qw/should_wordwrap private/) {
+        if (exists($m->{$_})) {
+            $m->{$_} = to_boolean($m->{$_});
+        }
+    }
+
+    # I don't care about ZAUTH_FAILED vs. ZAUTH_NO, so I'm just going
+    # to collapse them
+    if (exists($m->{'auth'})) {
+        $m->{'auth'} = to_boolean($m->{'auth'} == 'YES');
+    }
+
+    if ($m->{'login'} eq 'none') {
+        delete $m->{'login'};
     }
 
     $messages->insert($m);
